264fb3db1f11da4572c827cb2598b32bdee0dee6
Jing Zhang <zj391120@163.com>
2021-01-24 07:20:03 +0000

    version 1.7.0.0

--- R/knife.R
+++ R/knife.R
@@ -10,11 +10,17 @@
 #' knife_right(123,2)
 knife_left <- function(x,n){
     substr(x, n+1, nchar(x))
+    
 }
 #' @rdname knife
 #' @export
 knife_right <- function(x,n){
-    x=rev(strsplit(as.character(x),'')[[1]])
-    x=rev(x[-c(1:n)])
-    paste0(x,collapse = '')
+    knife_i <- function(x,n){
+        x=rev(strsplit(as.character(x),'')[[1]])
+        x=rev(x[-c(1:n)])
+        paste0(x,collapse = '')
+    }
+    x1=sapply(x, function(i) knife_i(i,n))
+    names(x1)=names(x)
+    x1
 }
